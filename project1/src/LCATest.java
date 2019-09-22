07657362f356e1b9ca44851c6679720b1486b3b0
Shengyuan <lius8@tcd.ie>
2019-09-22 12:11:30 +0100

    try the first test

--- project1/src/LCATest.java
+++ project1/src/LCATest.java
@@ -1,12 +1,27 @@
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
+
+@RunWith(JUnit4.class)
 public class LCATest {
-
+    
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testConstructor()
+	{
+	       new LCA();
+	}
+	@Test
+	public void testEmpty() {
+	        
 	}
 
 }
