53a370ee0fee4cf0995c7253dd063dc15348139c
Shengyuan <lius8@tcd.ie>
2019-09-29 10:30:31 +0100

    add the test only left or right root side

--- project1/src/LCATest.java
+++ project1/src/LCATest.java
@@ -26,11 +26,28 @@ public class LCATest {
 	}
 	
 	@Test
-	public void test1SideTree ()
+	public void testN1Tree ()
 	{
 		LCA tree = new LCA();
 		tree.root = new Node (5);
 		assertEquals("LCA can't search one side tree", -1, tree.getTheLCA(5, 3));
+	}
+	
+	@Test
+	public void testLeft1SideTree()
+	{
+		LCA tree =new LCA();
+		tree.root=new Node(1);
+		tree.root.left=new Node(2);
+		assertEquals("LCA of the(1,2) is", 1, tree.getTheLCA(1, 2));
+	}
+	@Test
+	public void testRight1SideTree()
+	{
+		LCA tree = new LCA();
+		tree.root =new Node(2);
+		tree.root.right=new Node(5);
+		assertEquals("LCA of the(2,5) is", 2, tree.getTheLCA(2, 5));
 	}
 	
 	
