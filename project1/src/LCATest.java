02a9878a0c2e111d594b0b6f3488c8721ecb0bb2
Shengyuan <lius8@tcd.ie>
2019-10-08 17:55:51 +0100

    add the first test,not sure if it's right for DAG

--- project1/src/LCATest.java
+++ project1/src/LCATest.java
@@ -237,6 +237,36 @@ public class LCATest {
     	assertEquals("Find LCA of non-existent node",tree.getTheLCA(11,12),-1);
 	}
 	
-	
+	// the binary tree's shape:
+		//          ---------
+		//             tree:
+		//               1
+		//              / \
+		//             2   3
+		//            /    /
+		//           4    5  
+	    //            \  /
+		//             6        
+		//             \         
+		//              7         
+		
+		@Test
+		public void testDAG()
+		{
+			LCA tree = new LCA(); 
+	        tree.root = new Node(1); 
+	        tree.root.left = new Node(2); 
+	        tree.root.right =new Node(3);
+	        tree.root.left.left = new Node(4);
+	        tree.root.right.left =new Node(5);
+	        tree.root.left.left.right= new Node(6);
+	        tree.root.right.left.left= tree.root.left.left.right;
+	        tree.root.left.left.right.right =new Node(7);
+	        
+	        assertEquals("Find LCA(4,7). Should return 4.",tree.getTheLCA(4,7),4);
+	        assertEquals("Find LCA(6,7). Should return 6.",tree.getTheLCA(6,7),6);
+	        assertEquals("Find LCA(4,5). Should return 1.",tree.getTheLCA(4,5),1);
+	    	assertEquals("Find LCA of non-existent node",tree.getTheLCA(11,12),-1);
+		}
 
 }
--- project2/src/LCAForDGATest.java
+++ project2/src/LCAForDGATest.java
@@ -4,9 +4,42 @@ import org.junit.Test;
 
 public class LCAForDGATest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+	    //add the 
+		@Test
+		public void testConstructor()
+		{
+		       new LCAForDGA();
+		}
+	// the binary tree's shape:
+			//          ---------
+			//             tree:
+			//               1
+			//              / \
+			//             2   3
+			//            /    /
+			//           4    5  
+		    //            \  /
+			//             6        
+			//             \         
+			//              7         
+			
+			@Test
+			public void testDAG()
+			{
+				LCAForDGA tree = new LCAForDGA(); 
+		        tree.root = new Node(1); 
+		        tree.root.left = new Node(2); 
+		        tree.root.right =new Node(3);
+		        tree.root.left.left = new Node(4);
+		        tree.root.right.left =new Node(5);
+		        tree.root.left.left.right= new Node(6);
+		        tree.root.right.left.left= tree.root.left.left.right;
+		        tree.root.left.left.right.right =new Node(7);
+		        
+		        assertEquals("Find LCA(4,7). Should return 4.",tree.getTheLCA(4,7),4);
+		        assertEquals("Find LCA(6,7). Should return 6.",tree.getTheLCA(6,7),6);
+		        assertEquals("Find LCA(4,5). Should return 1.",tree.getTheLCA(4,5),1);
+		    	assertEquals("Find LCA of non-existent node",tree.getTheLCA(11,12),-1);
+			}
 
 }
