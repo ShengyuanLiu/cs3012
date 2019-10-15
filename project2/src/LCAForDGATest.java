86df912c384015531afc720dcb612fc35cce1015
Shengyuan <lius8@tcd.ie>
2019-10-15 16:48:12 +0100

    add the DAG test

--- project2/src/LCAForDGATest.java
+++ project2/src/LCAForDGATest.java
@@ -1,45 +1,61 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class LCAForDGATest {
 
 	    //add the 
-		@Test
-		public void testConstructor()
-		{
-		       new LCAForDGA();
-		}
-	// the binary tree's shape:
-			//          ---------
-			//             tree:
-			//               1
-			//              / \
-			//             2   3
-			//            /    /
-			//           4    5  
-		    //            \  /
-			//             6        
-			//             \         
-			//              7         
-			
-			@Test
-			public void testDAG()
-			{
-				LCAForDGA tree = new LCAForDGA(); 
-		        tree.root = new Node(1); 
-		        tree.root.left = new Node(2); 
-		        tree.root.right =new Node(3);
-		        tree.root.left.left = new Node(4);
-		        tree.root.right.left =new Node(5);
-		        tree.root.left.left.right= new Node(6);
-		        tree.root.right.left.left= tree.root.left.left.right;
-		        tree.root.left.left.right.right =new Node(7);
-		        
-		        assertEquals("Find LCA(4,7). Should return 4.",tree.getTheLCA(4,7),4);
-		        assertEquals("Find LCA(6,7). Should return 6.",tree.getTheLCA(6,7),6);
-		        assertEquals("Find LCA(4,5). Should return 1.",tree.getTheLCA(4,5),1);
-		    	assertEquals("Find LCA of non-existent node",tree.getTheLCA(11,12),-1);
-			}
+
+	LCAForDGA theGraph = new LCAForDGA();
+	
+	//      1
+	//     / \
+	//    2   4
+	//   /|\  |
+	//  3 5 9 8
+	// /  |   | \
+	//6   7   11 10
+	//|   
+	//12
+	@Before
+	public void setup() {
+		theGraph.add(1);
+		theGraph.add(1,2);
+		theGraph.add(1,4);
+		theGraph.add(2,3);
+		theGraph.add(2,5);
+		theGraph.add(2,9);
+		theGraph.add(3,6);
+		theGraph.add(5,7);
+		theGraph.add(6,12);
+		theGraph.add(4,8);
+		theGraph.add(8,11);
+		theGraph.add(1,7);
+		theGraph.add(3,12);
+		theGraph.add(2,12);
+		theGraph.add(8,10);
+		theGraph.add(4,10);
+		theGraph.add(1,12);
+	}
+	
+	@Test
+	public void testContains() {
+		//to test if the vertex 9 is in the graph
+		assertTrue(theGraph.contains(9));
+		//to test if the vertex 12 is in the graph
+	    assertTrue(theGraph.contains(12));
+		//to test if the vertex 1 is in the graph
+		assertTrue(theGraph.contains(1));
+		//to test if the vertex 8 is in the graph
+		assertTrue(theGraph.contains(8));
+		
+        //test the not exist node
+		assertFalse(theGraph.contains(15));
+
+		assertFalse(theGraph.contains(-1));
+		
+		assertFalse(theGraph.contains(0));
+	}
 
 }
